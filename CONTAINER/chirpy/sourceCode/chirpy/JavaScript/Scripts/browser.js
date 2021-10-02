fc7179d3659b156e26d202a42c60099c51428330
John Holland <gunerb1@tcnj.edu>
2021-10-02 09:01:16 +0000

    update

--- CONTAINER/chirpy/sourceCode/chirpy/JavaScript/Scripts/browser.js
+++ CONTAINER/chirpy/sourceCode/chirpy/JavaScript/Scripts/browser.js
@@ -1,137 +1,152 @@
 var document = {
     byid: {},
     nodeType: 9,
-    getElementById: function(id){ return this.byid[id]; },
-    getElementsByTagName: function(name){
+    getElementById: function (id) {
+      return this.byid[id];
+    },
+    getElementsByTagName: function (name) {
       return this.body.getElementsByTagName(name);
     },
-    addEventListener: function(){ },
-    removeEventListener: function(){ },
-    createElement: function(tag){
+    addEventListener: function () {},
+    removeEventListener: function () {},
+    createElement: function (tag) {
       return new fragment(tag);
     },
-    createComment: function(){ return {} }    
+    createComment: function () {
+      return {};
+    },
   },
   window = this,
-  navigator = { userAgent: 'Mozilla/5.0'};
+  navigator = { userAgent: "Mozilla/5.0" };
 
-function fragment (nodeName, nodeType){
-        //if(this===window) return new fragment(nodeName);
+function fragment(nodeName, nodeType) {
+  //if(this===window) return new fragment(nodeName);
 
-        this.nodeName = (nodeName || 'none').toLowerCase(); 
-        this.nodeType = nodeType || 1;
-        this.style = {};
-        this.attributes = {}; 
-        this.className = '';
+  this.nodeName = (nodeName || "none").toLowerCase();
+  this.nodeType = nodeType || 1;
+  this.style = {};
+  this.attributes = {};
+  this.className = "";
 
-        this.childNodes = [];
+  this.childNodes = [];
 
-        if(nodeName === 'a') {
-            Object.defineProperty(this, 'href', { 
-                get: function(){ return 'http://localhost/'; },
-                set: function(val){ }
-            });    
-        } 
+  if (nodeName === "a") {
+    Object.defineProperty(this, "href", {
+      get: function () {
+        return "http://localhost/";
+      },
+      set: function (val) {},
+    });
+  }
 
-        var innerHTML = ''
-        Object.defineProperty(this, 'innerHTML', { 
-            get: function(){ return innerHTML; },
-            set: function(val){ innerHTML = val; }
-        });    
+  var innerHTML = "";
+  Object.defineProperty(this, "innerHTML", {
+    get: function () {
+      return innerHTML;
+    },
+    set: function (val) {
+      innerHTML = val;
+    },
+  });
 }
 
 fragment.prototype = {
-    createDocumentFragment: function(){ return new fragment('%fragment%', 11); },
-    getElementsByTagName: function(name){ 
-      name = (name || '').toLowerCase();
-      return this.getall().filter(function(elm){ return elm.nodeName == name });
-    },
-    
-    getElementsByClassName: function(name){
-      return this.getall().filter(function(elm){ return (elm.className||'').split(' ').indexOf(name) > -1; });
-    },
+  createDocumentFragment: function () {
+    return new fragment("%fragment%", 11);
+  },
+  getElementsByTagName: function (name) {
+    name = (name || "").toLowerCase();
+    return this.getall().filter(function (elm) {
+      return elm.nodeName == name;
+    });
+  },
 
-    appendChild: function(child){  
-      if(child.parentNode){
-        child.parentNode.removeChild(child);
-      }
+  getElementsByClassName: function (name) {
+    return this.getall().filter(function (elm) {
+      return (elm.className || "").split(" ").indexOf(name) > -1;
+    });
+  },
 
-      child.parentNode = this;
-      this.childNodes.push(child);
-    },
+  appendChild: function (child) {
+    if (child.parentNode) {
+      child.parentNode.removeChild(child);
+    }
 
-    cloneNode: function(deep){
-        var other = new fragment(this.nodeName, this.nodeType);
-        this.childNodes.forEach(function(child){
-            other.appendChild(child.cloneNode(deep));
-        });
-    },
-    
-    insertBefore: function(newchild, child){ 
-      var i = this.childNodes.indexOf(child);
-      if(i>-1) {
-        this.childNodes.splice(i, 0, child);
-        newchild.parentNode = this;
-      } else this.appendChild(newchild);
-    },
+    child.parentNode = this;
+    this.childNodes.push(child);
+  },
 
-    removeChild: function(child) {
-      if(!child) return;
-      var i = this.childNodes.indexOf(child);
-      if(i>-1) {
-        this.childNodes.splice(i, 1);
-        child.parentNode = undefined;
-      }
-    },
+  cloneNode: function (deep) {
+    var other = new fragment(this.nodeName, this.nodeType);
+    this.childNodes.forEach(function (child) {
+      other.appendChild(child.cloneNode(deep));
+    });
+  },
 
-    setAttribute: function(name, value){
-      this.attributes[name] = value;
-    },
-    
-    getAttribute: function(name){
-      return this.attributes[name];
-    },
+  insertBefore: function (newchild, child) {
+    var i = this.childNodes.indexOf(child);
+    if (i > -1) {
+      this.childNodes.splice(i, 0, child);
+      newchild.parentNode = this;
+    } else this.appendChild(newchild);
+  },
 
-    getall: function() {
-      var ret = [this];
-      this.childNodes.forEach(function(child){
-        if(child.getall) ret.push.apply(ret, child.getall());
-      });     
-      return ret;
-    },
-     
-    get lastChild(){
-        return this.childNodes[this.childNodes.length - 1];
-    },
+  removeChild: function (child) {
+    if (!child) return;
+    var i = this.childNodes.indexOf(child);
+    if (i > -1) {
+      this.childNodes.splice(i, 1);
+      child.parentNode = undefined;
+    }
+  },
 
-    get firstChild(){
-        return this.childNodes[0];
-    },
+  setAttribute: function (name, value) {
+    this.attributes[name] = value;
+  },
 
-    get id(){
-        return this.getAttribute('id');
-    }, 
-    set id(value){        
-        this.setAttribute('id', value);
-        document.byid[value] = this;
-    }, 
-    
-    get rel(){
-        return this.getAttribute('rel');
-    }, 
-    set rel(value){        
-        this.setAttribute('rel', value); 
-    }, 
-    
-    get className(){
-        return this.getAttribute('className');
-    }, 
-    set className(value){        
-        this.setAttribute('className', value); 
-    }            
-}
+  getAttribute: function (name) {
+    return this.attributes[name];
+  },
 
-document.documentElement = document.body = new fragment('body');
-window.document = document; 
-window.addEventListener = function(){ }
- 
+  getall: function () {
+    var ret = [this];
+    this.childNodes.forEach(function (child) {
+      if (child.getall) ret.push.apply(ret, child.getall());
+    });
+    return ret;
+  },
+
+  get lastChild() {
+    return this.childNodes[this.childNodes.length - 1];
+  },
+
+  get firstChild() {
+    return this.childNodes[0];
+  },
+
+  get id() {
+    return this.getAttribute("id");
+  },
+  set id(value) {
+    this.setAttribute("id", value);
+    document.byid[value] = this;
+  },
+
+  get rel() {
+    return this.getAttribute("rel");
+  },
+  set rel(value) {
+    this.setAttribute("rel", value);
+  },
+
+  get className() {
+    return this.getAttribute("className");
+  },
+  set className(value) {
+    this.setAttribute("className", value);
+  },
+};
+
+document.documentElement = document.body = new fragment("body");
+window.document = document;
+window.addEventListener = function () {};
--- CONTAINER/chirpy/sourceCode/chirpy/JavaScript/Scripts/env.js
+++ CONTAINER/chirpy/sourceCode/chirpy/JavaScript/Scripts/env.js
@@ -1,25 +1,27 @@
-﻿var window = {},sys = {
-    debug: function () { }
-}, process = {
+﻿var window = {},
+  sys = {
+    debug: function () {},
+  },
+  process = {
     argv: [],
-    exit: function () { }
-},
-require = (function () {
+    exit: function () {},
+  },
+  require = (function () {
     var exported = {};
     var files = {};
 
     return function (file) {
-        file = getFullFilename(file);
-        var key = file.toLowerCase();
+      file = getFullFilename(file);
+      var key = file.toLowerCase();
 
-        if (files[key]) {
-            return exported[key];
-        }
-
-        var js = getContents(file);
-        exported[key] = {}
-        files[key] = true;
-        (new Function('var exports = arguments[0]; ' + js + ';'))(exported[key]);
+      if (files[key]) {
         return exported[key];
-    }
-})();
+      }
+
+      var js = getContents(file);
+      exported[key] = {};
+      files[key] = true;
+      new Function("var exports = arguments[0]; " + js + ";")(exported[key]);
+      return exported[key];
+    };
+  })();
